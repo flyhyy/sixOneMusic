67ce2e6ea5001e719ac97f7d69de7cf903e157eb
MrHuang <newflyhuang@gmail.com>
2026-09-10 17:17:22 +0800

    前端：Toast 弹窗样式适配主题，并忽略后端运行产生的数据库文件

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import "vue-toastification/dist/index.css";
+import './style.css'
 
 import App from './App.vue'
 import Router from './router/index.ts'
