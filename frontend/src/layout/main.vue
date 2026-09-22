975944c48883566c08f0afb7723c4dd67d4c1ae3
MrHuang <newflyhuang@gmail.com>
2026-09-22 15:39:14 +0800

    调整主布局样式：为主容器添加高度以支持垂直滚动

--- frontend/src/layout/main.vue
+++ frontend/src/layout/main.vue
@@ -1,7 +1,7 @@
 /** * @Author: hyy * @Date: 2026-06-04 15:56:17 * @Description: 主要 */
 
 <template>
-  <main class="w-full p-6 overflow-x-hidden bg-canvas">
+  <main class="w-full h-full p-6 overflow-x-hidden overflow-y-auto bg-canvas">
     <slot></slot>
   </main>
 </template>
